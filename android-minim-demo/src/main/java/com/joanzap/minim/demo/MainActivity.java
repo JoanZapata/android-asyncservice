d2ad7a7e3a7270259d6c0f490011d16512d04dd4
Joan <zapata.joan@gmail.com>
2014-05-01 23:53:21 +0200

    Removed unused things in demo app

--- android-minim-demo/src/main/java/com/joanzap/minim/demo/MainActivity.java
+++ android-minim-demo/src/main/java/com/joanzap/minim/demo/MainActivity.java
@@ -2,8 +2,6 @@ package com.joanzap.minim.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,25 +9,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
